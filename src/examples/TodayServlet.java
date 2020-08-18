8598cbf02d93f39a22c410d678c0a4f0b998fb10
kimmy100b <kimmy100b@gmail.com>
2020-08-18 09:38:30 +0900

    코드리뷰 후 수정사항

--- src/examples/TodayServlet.java
+++ src/examples/TodayServlet.java
@@ -16,30 +16,19 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TodayServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		out.print("<a href='index.html'>메인화면</a>");
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm");
-		String nowString = now.format(dateTimeFormatter); 
-		out.print("<h1>현재시간 : ");
-		out.print(nowString);
-		out.print("</h1>");
-	}
-
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+  	PrintWriter out = response.getWriter();
+  	out.print("<a href='index.html'>메인화면</a>");
+  	String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd hh:mm"));
+  	out.print("<h1>현재시간 : ");
+  	out.print(nowString);
+  	out.print("</h1>");
+  }
 }
